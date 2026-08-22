a8b4ce3a469b46dfa1e7b6f0c31d344dfe30537d
Sohom-Paul <taraknathp931@gmail.com>
2026-08-22 08:10:09 +0530

    Add home page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome to CALMFLOW</h1>
+      <p className="text-xl mt-4">Your meditation app</p>
+    </main>
+  );
+}
